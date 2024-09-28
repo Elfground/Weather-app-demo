c5594733afe1f4859ada445da8157eb860fb9800
Elfground <elfground@gmail.com>
2024-09-28 23:54:39 +0200

    Added axios and more javascript

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
+function refreshWeather(response) {
+
+}
+
+function searchCity(city) {
+    let apiKey = "2f78437a500ef24fc3e9894233eftb0o";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML = searchInput.value;
-
+    searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
