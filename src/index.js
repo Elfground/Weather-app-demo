5c8054c6bf98a6e5c98fde37df03d5c7d8efbddc
Elfground <elfground@gmail.com>
2024-09-29 10:09:11 +0200

    updated javascript and html files with weather conditions, windspeed and humidity

--- src/index.js
+++ src/index.js
@@ -2,9 +2,29 @@ function refreshWeather(response) {
     let temperatureElement = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#city");
+    let conditionElement = document.querySelector("#weather-conditions");
+    let humidityElement = document.querySelector("#humidity");
+    let windspeedElement = document.querySelector("#windspeed");
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
+
     cityElement.innerHTML = response.data.city;
+    timeElement.innerHTML = formatDate(date);
+    conditionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windspeedElement.innerHTML = `${response.data.wind.speed} km/h`;
     temperatureElement.innerHTML = Math.round(temperature);
 }
+
+function formatDate(date) {
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days [date.getDay()];
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+ }
 
 function searchCity(city) {
     let apiKey = "2f78437a500ef24fc3e9894233eftb0o";
